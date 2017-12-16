fa5366f06378787cc7b8a59da1f0846bf035d6ee
Turbinist <s-rudyjj@rambler.ru>
2017-12-17 01:06:41 +0200

    Issue with reading file

--- Week_2/storage.py
+++ Week_2/storage.py
@@ -3,6 +3,12 @@ import tempfile
 import argparse
 from collections import OrderedDict
 import json
+
+#Path to temp directory
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+print(storage_path)
+
+#Take argument from shell
 parser = argparse.ArgumentParser()
 parser.add_argument("--key")
 parser.add_argument("--val")
@@ -10,19 +16,38 @@ args = parser.parse_args()
 answer = [args.key ,args.val]
 print(answer)
 
+
 def store(st, key, val):
-	store_dict = OrderedDict(st)
-	if key in store_dict:
-		store_dict[key].append(val)
-	else:
-		store_dict[key] = [val]
-	return store_dict
+    """Save to dictionary key with value. If key in dictionary, append to list."""
+    store_dict = OrderedDict(st)
+    if key in store_dict:
+        store_dict[key].append(val)
+    else:
+        store_dict[key] = [val]
+    return store_dict
 	
+	
+def file_r_w(path, key, val):
+    """Open file and write dictionary to file in json_format"""
+    with open(path, 'a+') as f:
+        fff = f.read()
+        print('In file: ', fff)
+        if len(fff) < 2:
+            print(len(fff))
+            dictionary=store({}, key, val)
+            f.seek(0)
+            json.dump(dictionary, f)
+        else:
+            data = json.loads(fff)
+            with open(path, 'w') as f:
+                json.dump(store(data, key, val), f)
+        f.close()
 print(store({'name':['Alex'], 'surname':['Alexsandr'], 'nick':['Alex444']}, args.key, args.val))
-	
-	
+print(file_r_w(storage_path, args.key, args.val))	    
+
+		
+		
 """
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 print(storage_path)
 
 with open(storage_path, 'a') as empty_file:
