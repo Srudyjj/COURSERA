0caa4d7836432081c8accf10102bbb0027d898be
Turbinist <s-rudyjj@rambler.ru>
2017-12-31 21:59:23 +0200

    git

---
+++ Week_3/solution.py
@@ -0,0 +1,11 @@
+class FileReader:
+    def __init__(self, path_to_file):
+        self.path_to_file = path_to_file
+
+    def read(self):
+        try:
+            with open(self.path_to_file, "r+") as f:
+                return f.read()
+                
+        except IOError:
+            return ""
