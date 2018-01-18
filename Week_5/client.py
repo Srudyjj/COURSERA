4353f66c586fda932eee911c39fa4107507da9f9
Turbinist <s-rudyjj@eambler.ru>
2018-01-19 01:27:40 +0200

    Issues with bites in PUT

--- Week_5/client.py
+++ Week_5/client.py
@@ -1,4 +1,5 @@
 import time
+import socket
 
 class Client:
     curent_time = time.time()
@@ -7,14 +8,21 @@ class Client:
         self.port = port
         self.timeout = timeout
 
-    def get(key):
-        pass
+    def get(self, key):
+        with socket.create_connection((self.host, self.port),self.timeout) as sock:
+            sock.sendall(key.encode("utf8"))
+        #print(self.host, self.port ,self.timeout)
 
-    def put(key, value, timestamp=time.time()):
-        pass
-
-
+    def put(self, key, value, timestamp=curent_time):
+        with socket.create_connection((self.host, self.port),self.timeout) as sock:
+            message = b'{} {} {}\n'.format(key, value, int(timestamp))
+            #print(message)
+            sock.sendall(message.encode("utf8"))
+        
 class ClientError():
     pass
 
+
+#client = Client("127.0.0.1", 10001, timeout=15)
+#client.put("palm.cpu", 0.5)
         
