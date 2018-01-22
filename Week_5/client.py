9432588a8b542d5c7d1ef1a491721c00b7b57657
Turbinist <s-rudyjj@eambler.ru>
2018-01-22 17:44:21 +0200

    Task done!!!

--- Week_5/client.py
+++ Week_5/client.py
@@ -10,47 +10,57 @@ class Client:
         self.timeout = timeout
 
     def get(self, key):
-        with socket.create_connection((self.host, self.port),self.timeout) as sock:
-            message = 'get {}\n'.format(key)
-            sock.sendall(message.encode("utf8"))
-            received_data = sock.recv(1024)
-            return self._parser(received_data)
+        try:
+            with socket.create_connection((self.host, self.port),self.timeout) as sock:
+                message = 'get {}\n'.format(key)
+                sock.sendall(message.encode("utf8"))
+                received_data = sock.recv(1024)
+                return self._parser(received_data)
+        except socket.error:
+            raise ClientError("error\nwrong command\n\n")    
             
 
     def _parser(self, inner_message):
         """Convert inner bite string to dictionary"""
         inner = inner_message.decode("utf8")
         new_dict = dict()
-        if inner == 'ok\n\n':
-            return new_dict
-        else:
-            data_sring = inner.replace("ok\n","").replace("\n\n","").splitlines()
-            data_list = []
-            for line in data_sring:
-                splitted_line = line.lstrip().split()
-                data_list.append(splitted_line)
-            for string in data_list:
-                val = (int(string[2]), float(string[1]),)
-                if string[0] in new_dict:
-                    new_dict[string[0]].append(val)
-                else:
-                    new_dict[string[0]] = [val]
-            return new_dict
+        try:
+            if inner == 'ok\n\n':
+                return new_dict
+            else:
+                data_sring = inner.replace("ok\n","").replace("\n\n","").splitlines()
+                data_list = []
+                for line in data_sring:
+                    splitted_line = line.lstrip().split()
+                    data_list.append(splitted_line)
+                for string in data_list:
+                    val = (int(string[2]), float(string[1]),)
+                    if string[0] in new_dict:
+                        new_dict[string[0]].append(val)
+                    else:
+                        new_dict[string[0]] = [val]
+                return new_dict
+        except IndexError:
+            raise ClientError("error\nwrong command\n\n")
 
 
 
             
 
     def put(self, key, value, timestamp=curent_time):
-        with socket.create_connection((self.host, self.port),self.timeout) as sock:
-            message = 'put {} {} {}\n'.format(key, value, int(timestamp))
-            sock.sendall(message.encode("utf8"))
-            received_data = sock.recv(1024)
-            return received_data.decode("utf8")            
+        try:
+            with socket.create_connection((self.host, self.port),self.timeout) as sock:
+                message = 'put {} {} {}\n'.format(key, value, int(timestamp))
+                sock.sendall(message.encode("utf8"))
+                received_data = sock.recv(1024)
+                return received_data.decode("utf8")
+        except socket.error:
+            raise ClientError("error\nwrong command\n\n")
+
         
-class ClientError():
+class ClientError(Exception):
     pass
 
 
-client = Client("127.0.0.1", 10001, timeout=150)
-print(client.get("*"))
+# client = Client("127.0.0.1", 10001, timeout=150)
+# print(client.put("palm.cpu", 0.5, timestamp=1150864247))
